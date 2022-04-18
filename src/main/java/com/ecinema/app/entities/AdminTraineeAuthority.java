c8aa0e437381a9c712ff203f518de5f192f2a9ca
JohnLavender474 <jml62608@uga.edu>
2022-04-17 22:02:46 -0400

    Second commit, working on entity relationships

--- src/main/java/com/ecinema/app/entities/AdminTraineeAuthority.java
+++ src/main/java/com/ecinema/app/entities/AdminTraineeAuthority.java
@@ -3,13 +3,21 @@ package com.ecinema.app.entities;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Getter
 @Setter
 @Entity
 public class AdminTraineeAuthority extends AbstractEntity {
 
+    @Column
+    private Integer percentageTrainingModulesCompleted;
 
+    @ManyToOne
+    @JoinColumn
+    private AdminAuthority mentor;
 
 }
